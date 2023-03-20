9e65fbf3d96eb3fa028de8687dd43d97a718e296
laijunjie <jjlai0112@gmail.com>
2023-03-20 22:17:19 +0800

    fix: typing error in customer list model

--- WhatsappBlasterSystem/app/Models/CustomerList.php
+++ WhatsappBlasterSystem/app/Models/CustomerList.php
@@ -5,9 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\BlastingList;
 
 class CustomerList extends Model
 {
     use HasFactory, SoftDeletes;
-    protected $fillable = ['user_id','blasting_id','attribute1','attribute2','attribute3','attribute4','attribute5','attribute6',,'attribute7'];
+    protected $fillable = ['user_id','blasting_id','attribute1','attribute2','attribute3','attribute4','attribute5','attribute6','attribute7'];
+
+    public function blastingLists()
+    {
+        return $this->belongsTo(BlastingList::class,'id','blasting_id');
+    }
+
+
 }
