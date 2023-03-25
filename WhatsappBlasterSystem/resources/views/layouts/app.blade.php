c136a9f29c24459e9d18e06ebdf8b77d141436e0
laijunjie <jjlai0112@gmail.com>
2023-03-25 14:04:15 +0800

    improvement: table css

--- WhatsappBlasterSystem/resources/views/layouts/app.blade.php
+++ WhatsappBlasterSystem/resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js','resources/css/app.css'])
 </head>
 @if (Session::has('error'))
     <!---for error user alert--->
--- WhatsappBlasterSystem/resources/views/user/message/index.blade.php
+++ WhatsappBlasterSystem/resources/views/user/message/index.blade.php
@@ -1,45 +1,49 @@
 @extends('layouts.app')
 @section('content')
-    <div class="row">
-        <div class="col-sm-3"></div>
-        <div class="col-sm-6">
-            <br><br>
-            <table class="table table-bordered" id="myTable">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Meesage</th>
-                        <th>List Name</th>
-                        <th>Send time</th>
-                        <th>Phone</th>
-                        <th>Operates</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @if ($messages->isNotEmpty())
-                        @foreach ($messages as $key=>$message)
-                            <tr>
-                                <td>{{$key+1}}</td>
-                                <td>{{ $message->message }}</td>
-                                <td>{{ $message->blasters->name }}</td>
-                                <td>{{ $message->send_time }}</td>
-                                <td>{{ $message->phone }}</td>
-                                <td><a href="{{ route('message_edit_view', ['id' => $message->id]) }}"
-                                        class="btn btn-primary btn-xs">Edit</a>
-                                    <a href="{{ route('message_delete', ['id' => $message->id]) }}"
-                                        onClick="return confirm('Are you sure to delete?')"
-                                        class="btn btn-primary btn-xs">Delete</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    @else
-                        <tr>
-                            <td colspan="3" class="text-center">Add Message List now!</td>
-                        </tr>
-                    @endif
-                </tbody>
-            </table>
+    <div class="container">
+        <div class="row">
+            <div class="card p-0 rounded-4">
+                <div class="card-header">
+                    <h1 class="text-center mb-md-3 mt-md-3">Message List</h1>
+                </div>
+                <div class="p-4">
+                        <table id="myTable">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Meesage</th>
+                                    <th>List Name</th>
+                                    <th>Send time</th>
+                                    <th>Phone</th>
+                                    <th>Operates</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if ($messages->isNotEmpty())
+                                    @foreach ($messages as $key => $message)
+                                        <tr>
+                                            <td>{{ $key + 1 }}</td>
+                                            <td>{{ $message->message }}</td>
+                                            <td>{{ $message->blasters->name }}</td>
+                                            <td>{{ $message->send_time }}</td>
+                                            <td>{{ $message->phone }}</td>
+                                            <td><a href="{{ route('message_edit_view', ['id' => $message->id]) }}"
+                                                    class="btn btn-secondary btn-xs">Edit</a>
+                                                <a href="{{ route('message_delete', ['id' => $message->id]) }}"
+                                                    onClick="return confirm('Are you sure to delete?')"
+                                                    class="btn btn-danger btn-xs">Delete</a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <tr>
+                                        <td colspan="6" class="text-center">Add Message List now!</td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                </div>
+            </div>
         </div>
-        <div class="col-sm-3"></div>
     </div>
 @endsection
