bdc7ed3d441ee1bfe249095c9a8801e5eed5f0f1
laijunjie <jjlai0112@gmail.com>
2023-03-26 22:33:06 +0800

    LAST update

--- WhatsappBlasterSystem/database/seeders/DatabaseSeeder.php
+++ WhatsappBlasterSystem/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,11 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\OnsendApi;
+use App\Models\Blaster;
+use App\Models\Customer;
+use App\Models\Message;
+
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +19,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //default user
         User::create([
             'name' => 'SuperAdmin',
             'email' => 'SuperAdmin@gmail.com',
@@ -36,6 +42,62 @@ class DatabaseSeeder extends Seeder
             'role' => 0,
             'phone' => '60123456789',
             'password' => Hash::make('user21234'),
+        ]);
+        //default onsend api
+        OnsendApi::create([
+            'user_id' => '2',
+            'api' => 'd5bd09371f31fd7c8efa78d1732d24f9e3f96a8643dd05f6075dedcd19c05452',
+        ]);
+
+        //default blaster
+        Blaster::create([
+            'user_id' => '2',
+            'name' => 'Test1',
+        ]);
+        Blaster::create([
+            'user_id' => '2',
+            'name' => 'Test2',
+        ]);
+
+        //default customer
+        Customer::create([
+            'blaster_id' => '1',
+            'attribute1' => 'jj1',
+            'attribute2' => '0143886279',
+            'attribute3' => 'address1',
+            'attribute4' => '19-7-2023',
+            'attribute5' => null,
+            'attribute6' => null,
+            'attribute7' => null,
+        ]);
+        Customer::create([
+            'blaster_id' => '1',
+            'attribute1' => 'jj2',
+            'attribute2' => '0143886279',
+            'attribute3' => 'address2',
+            'attribute4' => '09-10-2023',
+            'attribute5' => null,
+            'attribute6' => null,
+            'attribute7' => null,
+        ]);
+        Customer::create([
+            'blaster_id' => '1',
+            'attribute1' => 'jj3',
+            'attribute2' => '0169682362',
+            'attribute3' => 'address3',
+            'attribute4' => '15-5-2023',
+            'attribute5' => null,
+            'attribute6' => null,
+            'attribute7' => null,
+        ]);
+
+        //default message
+        Message::create([
+            'user_id' => '2',
+            'message' => 'Hi,[attribute1], your adrress is [attribute2], you road tax will be expire at [attribute4].',
+            'blaster_id' => '1',
+            'send_time' => '2023-04-5 12:09:00',
+            'phone' => '[attribute2]'
         ]);
     }
 }
