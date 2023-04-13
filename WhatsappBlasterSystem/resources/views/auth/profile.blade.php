bb204193c804d4f52ab68e791b8c228623c59dcf
laijunjie <jjlai0112@gmail.com>
2023-04-13 21:49:38 +0800

    improvement: add background color in profile and api setting page

--- WhatsappBlasterSystem/resources/views/auth/profile.blade.php
+++ WhatsappBlasterSystem/resources/views/auth/profile.blade.php
@@ -1,31 +1,40 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container col-sm-12   mt-3">
-        <h3>Profile</h3>
-        @if ($check == 'admin')
-            <form action="{{ route('profile_update_user') }}" method="POST" enctype="multipart/form-data">
-        @else
-            <form action="{{ route('profile_update') }}" method="POST" enctype="multipart/form-data">
-        @endif
-        @CSRF
-        <div class="form-group">
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-md-3"></div>
+            <div class="col col-12 col-md-6 mt-md-5 border p-2 p-md-5 rounded-5 form-background">
+                <h3>Profile</h3>
+                @if ($check == 'admin')
+                    <form action="{{ route('profile_update_user') }}" method="POST" enctype="multipart/form-data">
+                    @else
+                        <form action="{{ route('profile_update') }}" method="POST" enctype="multipart/form-data">
+                @endif
+                @CSRF
+                <div class="form-group">
 
-            <input class="form-control" type="hidden" id="id" name="id" value="{{ $profile->id }}">
-            <label for="name">Name</label>
-            <input class="form-control" type="text" id="name" name="name" value="{{ $profile->name }}" required>
-            <label for="email">Email</label>
-            <input class="form-control" type="text" id="email" name="email" value="{{ $profile->email }}" disabled>
-            <label for="email">Phone Number</label>
-            <input id="phone" type="phone" class="form-control @error('phone') is-invalid @enderror" name="phone"
-                value="{{ $profile->phone }}" required autocomplete="phone">
-            @error('phone')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+                    <input class="form-control" type="hidden" id="id" name="id" value="{{ $profile->id }}">
+                    <label for="name">Name</label>
+                    <input class="form-control" type="text" id="name" name="name" value="{{ $profile->name }}"
+                        required>
+                    <br>
+                    <label for="email">Email</label>
+                    <input class="form-control" type="text" id="email" name="email" value="{{ $profile->email }}"
+                        disabled>
+                    <br>
+                    <label for="email">Phone Number</label>
+                    <input id="phone" type="phone" class="form-control @error('phone') is-invalid @enderror"
+                        name="phone" value="{{ $profile->phone }}" required autocomplete="phone">
+                    @error('phone')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <br>
+                <button type="submit" class="btn btn-primary">Update</button>
+                </form>
+            </div>
         </div>
-
-        <button type="submit" class="btn btn-primary">Update</button>
-        </form>
-        <div>
-        @endsection
+    </div>
+@endsection
--- WhatsappBlasterSystem/resources/views/user/apiSetting.blade.php
+++ WhatsappBlasterSystem/resources/views/user/apiSetting.blade.php
@@ -1,31 +1,37 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container col-sm-12   mt-3">
-        <h3>On Send API Key</h3>
-        @if ($check == 'none')
-            <form action="{{ route('api_edit') }}" method="POST" enctype="multipart/form-data">
-                @CSRF
+            <div class="container">
+                <div class="row">
+                    <div class="col col-12 col-md-3"></div>
+                    <div class="col col-12 col-md-6 mt-md-5 border p-2 p-md-5 rounded-5 form-background">
+                        <h3>On Send API Key</h3>
+                        @if ($check == 'none')
+                            <form action="{{ route('api_edit') }}" method="POST" enctype="multipart/form-data">
+                                @CSRF
 
-                <div class="form-group">
-                    <label for="key">API Key</label>
-                    <input class="form-control" type="text" id="key" name="key" value="{{ $api }}"
-                        readonly>
+                                <div class="form-group">
+                                    <label for="key">API Key</label>
+                                    <input class="form-control" type="text" id="key" name="key"
+                                        value="{{ $api }}" readonly>
+                                </div>
+                                <br>
+                                <button type="submit" class="btn btn-primary">Edit</button>
+
+
+                            </form>
+                        @else
+                            <form action="{{ route('api_update') }}" method="POST" enctype="multipart/form-data">
+                                @CSRF
+                                <div class="form-group">
+                                    <label for="key">API Key</label>
+                                    <input class="form-control" type="text" id="key" name="key"
+                                        value="{{ $api }}" required>
+                                </div>
+                                <br>
+                                <button type="submit" class="btn btn-primary">Update</button>
+                            </form>
+                        @endif
+                    </div>
                 </div>
-
-                <button type="submit" class="btn btn-primary">Edit</button>
-
-
-            </form>
-        @else
-            <form action="{{ route('api_update') }}" method="POST" enctype="multipart/form-data">
-                @CSRF
-                <div class="form-group">
-                    <label for="key">API Key</label>
-                    <input class="form-control" type="text" id="key" name="key" value="{{ $api }}" required>
-                </div>
-
-                <button type="submit" class="btn btn-primary">Update</button>
-            </form>
-        @endif
-        <div>
-@endsection
+            </div>
+        @endsection
